1ee115834d2a1cfca80faf22fe4e33cfec167e58
beckspark <stephenpbeck@gmail.com>
2024-06-24 07:49:07 -0400

    Initial commit

---
+++ ex1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/* This is a comment. */
+int main(int argc, char *argv[])
+{
+	int distance = 100;
+
+	// this is also a comment
+	printf("You are %d miles away.\n That seems pretty far.\n How are you doing today?\n I needed something more complex.\n Will this work, you think?\n", distance);
+
+	return 0;
+}
