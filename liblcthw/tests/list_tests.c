adcfa4bf1eb27e22a2c6fbb3917a651c71c596c4
beckspark <stephenpbeck@gmail.com>
2024-10-30 09:26:19 -0400

    correct/reformat list_tests.c

--- liblcthw/tests/list_tests.c
+++ liblcthw/tests/list_tests.c
@@ -20,7 +20,7 @@ char *test_destroy()
     List_clear_destroy(list);
 
     return NULL;
-    
+
 }
 
 char *test_push_pop()
@@ -29,11 +29,11 @@ char *test_push_pop()
     mu_assert(List_last(list) == test1, "Wrong last value.");
 
     List_push(list, test2);
-    mu_assert(List_last(list) == test2, "Wrong last value.");
+    mu_assert(List_last(list) == test2, "Wrong last value");
 
-    List_push(list, test2);
+    List_push(list, test3);
     mu_assert(List_last(list) == test3, "Wrong last value.");
-    mu_assert(List_count(list) == 3,  "Wrong count on push.");
+    mu_assert(List_count(list) == 3, "Wrong count on push.");
 
     char *val = List_pop(list);
     mu_assert(val == test3, "Wrong value on pop.");
@@ -54,7 +54,7 @@ char *test_unshift()
     mu_assert(List_first(list) == test1, "Wrong first value.");
 
     List_unshift(list, test2);
-    mu_assert(List_first(list) == test2, "Wrong first value.");
+    mu_assert(List_first(list) == test2, "Wrong first value");
 
     List_unshift(list, test3);
     mu_assert(List_first(list) == test3, "Wrong last value.");
@@ -65,7 +65,7 @@ char *test_unshift()
 
 char *test_remove()
 {
-    // We only need  to test the middle remove case since push/shift
+    // we only need to test the middle remove case since push/shift 
     // already tests the other cases
 
     char *val = List_remove(list, list->first->next);
@@ -83,7 +83,7 @@ char *test_shift()
 
     char *val = List_shift(list);
     mu_assert(val == test3, "Wrong value on shift.");
-    
+
     val = List_shift(list);
     mu_assert(val == test1, "Wrong value on shift.");
     mu_assert(List_count(list) == 0, "Wrong count after shift.");
